14748be9a846af32a58d6a69c2cddcf6527d9f56
dorosh <dorosshh@gmail.com>
2015-08-03 21:40:40 +0300

    expand profile

--- apps/profiler/urls.py
+++ apps/profiler/urls.py
@@ -13,18 +13,13 @@ __date__ = '02.08.2015'
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
-from .views import (
-    MyRegistrationView, Login, UserPage
-)
+from .views import MyRegistrationView, UserPage
 
 
 urlpatterns = patterns(
     url(r'^user_page/(?P<pk>\d+)/$', UserPage.as_view(), name='user_page'),
-
-    # url(r'^login_auth/$', Login.as_view(), name='login_auth'),
     url(r'^register/$', MyRegistrationView.as_view(),
         name='registration_register2'),
-
     url(r'^profile/$', TemplateView.as_view(template_name='profile.html'),
         name='profile'),
 )
