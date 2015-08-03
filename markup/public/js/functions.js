3471790790fe38df0de2577b1c14feb866d4eda4
mkrishtopa <michael.krishtopa@gmail.com>
2015-08-03 16:11:58 +0300

    Add few static pages with SCSS+JS

---
+++ markup/public/js/functions.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+
+  $(".search-input").keypress(function(e) {
+    if (e.which == 13) {
+      e.preventDefault();
+      $(e.currentTarget).parent().parent().submit();
+    }
+  });
+
+  var ces = $(".competencies-educational-standard");
+  ces.find(".switcher > a").click(function(e) {
+    e.preventDefault();
+    ces.toggleClass("js-show-more js-show-less");
+  });
+
+  if ($(".section-course").length) {
+    $(".course-content-header").sticky({topSpacing:0});
+  };
+
+});
