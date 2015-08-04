be20650b2e42fb76a92986190b95373ff4921142
dorosh <dorosshh@gmail.com>
2015-08-04 15:43:15 +0300

    add email validation

--- apps/profiler/email.py
+++ apps/profiler/email.py
@@ -11,17 +11,21 @@ class EmailAuth(LegacyAuth):
     REQUIRES_EMAIL_VALIDATION = True
     EXTRA_DATA = ['email']
 
-    def auth_complete(self, args, *kwargs):
+    def auth_complete(self, user=None, *args, **kwargs):
         """
         Completes loging process, must return user instance.
         """
+
+        print user, args, kwargs
+
         if self.ID_KEY not in self.data:
             code = self.strategy.request.REQUEST.get('verification_code')
-            code_object = CustomCode.objects.filter(code=code).first()
-            if code_object:
-                email = code_object.email
-                self.data.update({'email': email})
-            else:
-                raise AuthMissingParameter(self, self.ID_KEY)
+            # code_object = CustomCode.objects.filter(code=code).first()
+            # if code_object:
+            #     email = code_object.email
+            #     self.data.update({'email': email})
+            # else:
+            #     raise AuthMissingParameter(self, self.ID_KEY)
+        self.data.update({'email': 'dorosshh@gmail.com'})#email})
         kwargs.update({'response': self.data, 'backend': self})
         return self.strategy.authenticate(*args, **kwargs)
