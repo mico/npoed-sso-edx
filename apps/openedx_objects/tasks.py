43ab40878cdd7114917dd44d718ee28efc5388b8
Maxim Starodubcev <maxim@trambroid.com>
2015-08-10 13:53:14 +0300

    Fix exception
    Delete files

--- apps/openedx_objects/tasks.py
+++ apps/openedx_objects/tasks.py
@@ -122,6 +122,6 @@ def _datetime_parser(dct):
             and re.search("\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z", v)):
             try:
                 dct[k] = datetime.datetime.strptime(v, '%Y-%m-%dT%H:%M:%SZ')
-            except:
+            except ValueError:
                 pass
     return dct
