ab9aaec78cbc9ad7bd1adddb80a0f3f9757d3350
Aleksey Radchenko <dixon.che@gmail.com>
2015-08-24 15:57:46 +0300

    Changed sync edx object with new course API with EDX_API_KEY

--- apps/openedx_objects/tasks.py
+++ apps/openedx_objects/tasks.py
@@ -5,36 +5,38 @@ from .models import EdxCourse, EdxOrg, EdxCourseRun, EdxCourseEnrollment
 from .utils import datetime_parser
 from apps.profiler.models import User
 
+from copy import copy
 import requests
 import json
+import logging
+log = logging.getLogger(__name__)
 
 
 _uoc_course = EdxCourse.objects.update_or_create
 _uoc_org = EdxOrg.objects.update_or_create
 _uoc_run = EdxCourseRun.objects.update_or_create
 _uoc_enrollment = EdxCourseEnrollment.objects.update_or_create
-_SESSION = requests.Session()
-#_SESSION.headers.update({'X-Edx-Api-Key': settings.EDX_API_TOKEN})
-_ENRL_SESSION = requests.Session()
-_ENRL_SESSION.headers.update({'X-Edx-Api-Key': settings.EDX_API_TOKEN})
 
 
 def get_edx_objects():
-    is_login = login()
-    if not is_login:
-        print 'Log in error'
-        return None
+    if not settings.EDX_API_KEY:
+        log.error("EDX_API_KEY settings should be specified!")
+        return
+
+    request = requests.Session()
+    request.headers.update({'X-Edx-Api-Key': settings.EDX_API_KEY})
 
     url = settings.EDX_COURSES_API
     params = {'format': 'json'}
-    enrl_params = params
+    enrl_params = copy(params)
     courses = []
     enrollments = []
     orgs = []
     runs = []
 
     while True:
-        r = _SESSION.get(url, params=params)
+        r = request.get(url, params=params)
+        # TODO: Check response code here!
         data = json.loads(r.text, object_hook=datetime_parser)
 
         for course in data.get('results', []):
@@ -56,7 +58,7 @@ def get_edx_objects():
             )
 
             if created:
-                print 'Course "%s" is created' % course['name']
+                log.info('Course "%s" is created' % course['name'])
 
             if course_obj.id not in courses:
                 courses.append(course_obj.id)
@@ -64,12 +66,12 @@ def get_edx_objects():
             run_obj, run_created = _uoc_run(name=course['run'], course=course_obj)
 
             if run_created:
-                print 'Course run "%s" is created' % course['run']
+                log.info('Course run "%s" is created' % course['run'])
 
             if run_obj.id not in runs:
                 runs.append(run_obj.id)
                 enrl_params['course_run'] = course_id
-                r = _ENRL_SESSION.get(settings.EDX_ENROLLMENTS_API, params=enrl_params)
+                r = request.get(settings.EDX_ENROLLMENTS_API, params=enrl_params)
                 result = json.loads(r.text, object_hook=datetime_parser)
                 enrollments.extend(add_enrollments(result, run_obj))
 
@@ -77,6 +79,7 @@ def get_edx_objects():
         if url is None:
             break
 
+    # TODO: Remove these objects only if all response codes from edx is 200
     EdxCourseEnrollment.objects.exclude(id__in=enrollments).delete()
     EdxCourseRun.objects.exclude(id__in=runs).delete()
     EdxCourse.objects.exclude(id__in=courses).delete()
@@ -103,29 +106,6 @@ def add_enrollments(result, run_obj):
             )
             enrollments.append(enrollment_obj.id)
             if created:
-                print 'Enrollment "%s - %s" is created' % (enrollment['user'],
-                                                            run_obj)
+                log.info('Enrollment "%s - %s" is created' % (enrollment['user'], run_obj))
 
     return enrollments
-
-
-def login():
-    r = _SESSION.get(settings.EDX_API_LOGIN_URL,
-                     params={'auth_entry': 'login', 'next': '/'})
-
-    post_login_data = {
-        'username': settings.EDX_API_USER,
-        'password': settings.EDX_API_PASS,
-        'csrfmiddlewaretoken': r.cookies.get('csrftoken'),
-    }
-
-    login_url = reverse('login')
-    domain = '/'.join(r.url.split('/')[:3])
-    querystring = '?'.join(r.url.split('?')[1:])
-    full_login_url = '%s%s?%s' % (domain, login_url, querystring)
-    cookies = r.cookies
-    r = _SESSION.post(full_login_url, data=post_login_data)
-    if not r.ok or domain in r.url:
-        return False
-
-    return True
