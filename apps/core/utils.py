8a70726b72f3ec49bcc51af5fa44f846baab7169
dorosh <dorosshh@gmail.com>
2015-08-28 13:53:02 +0300

    del home

--- apps/core/utils.py
+++ apps/core/utils.py
@@ -34,7 +34,7 @@ class SuperUserRequiredMixin(object):
 
     @classmethod
     def as_view(cls, **initkwargs):
-        view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
+        view = super(SuperUserRequiredMixin, cls).as_view(**initkwargs)
         return login_required(user_passes_test(lambda u: u.is_superuser)(view))
 
 
