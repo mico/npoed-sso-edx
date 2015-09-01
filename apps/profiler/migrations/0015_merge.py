e3f25d6660c4d667f13fd4542a8c49e21b052c8d
www-data <www-data@example.com>
2015-09-01 00:13:59 +0000

    Resolve migrate conflicts

---
+++ apps/profiler/migrations/0015_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('profiler', '0014_verbose_name_for_about_me_field'),
+        ('profiler', '0013_auto_20150831_1114'),
+    ]
+
+    operations = [
+    ]
