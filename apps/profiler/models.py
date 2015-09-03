1fbd512f9f59969c2f821669083bb8e7ef29ba94
Serge Pokhodyaev <snp@z9.ru>
2015-09-03 13:04:14 +0300

    we don't need SECURE_PROXY_SSL_HEADER

--- apps/profiler/models.py
+++ apps/profiler/models.py
@@ -83,9 +83,8 @@ class RegistrationProfile(BaseRegistrationProfile):
         # can overwrite some of the values like user
         # if django.contrib.auth.context_processors.auth is used
         prefix = 'http'
-        if settings.SECURE_PROXY_SSL_HEADER:
-            if settings.SECURE_PROXY_SSL_HEADER[1] == 'https':
-                prefix = 'https'
+        if request.META.get('HTTP_X_FORWARDED_PROTO'):
+            prefix = request.META.get('HTTP_X_FORWARDED_PROTO')
         ctx_dict.update({
             'user': self.user,
             'activation_key': self.activation_key,
