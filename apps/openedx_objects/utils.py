c5dbc8177d260fffff5a30248bce23fab97d1e2d
Aleksey Radchenko <dixon.che@gmail.com>
2015-09-11 17:38:06 +0300

    Added comment to not obvious code

--- apps/openedx_objects/utils.py
+++ apps/openedx_objects/utils.py
@@ -1,8 +1,13 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
 import re
 import datetime
 
 
 def datetime_parser(dct):
+    """
+    Используется для парсинга даты-времени в результатах API из edx
+    """
     for k, v in dct.items():
         if (isinstance(v, basestring)
             and re.search("\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z", v)):
